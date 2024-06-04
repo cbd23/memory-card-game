006eff16f3e051369eb5aef5deb8ff3cfe4bbcd8
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-06-04 20:10:38 +0300

    Import & add components for the 3 main sections.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { Fragment, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Header } from './components/Header'
+import { Main } from './components/Main'
+import { Footer } from './components/Footer'
 
 function App() {
 
   return (
-    <Fragment>
-      
-    </Fragment>
+    <div className='app-container'>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </div>
   )
 }
 
