13b07e321affc535870a7bb828ad18f96b625904
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-06-09 12:04:39 +0300

    Add 'Cards' inside Main.

---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+import './../styles/Main.css'
+import { Card } from './Card'
+
+export function Main() {
+    return (
+        <div className='main-container'>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+        </div>
+    )
+}
