a7b9bcdeb1ef082d2c7cb16ccb52b0e8f10306f2
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-06-10 17:04:31 +0300

    Remove placeholders & render a Card comp for each item inside cards.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,14 @@
+/* eslint-disable react/prop-types */
 import './../styles/Main.css'
 import { Card } from './Card'
 
-export function Main() {
+export function Main({ cards }) {
+
+    const listCards = cards.map(card => <Card key={card.id}>{card.name}</Card>)
+
     return (
         <div className='main-container'>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
+            {listCards}
         </div>
     )
 }
