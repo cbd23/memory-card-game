d1a728e5bbf19ab1190e18ae6b8bd706328c325a
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-06-18 16:07:23 +0300

    Add ev listener that triggers handleClick().

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import './../styles/Card.css'
 
-export function Card({ id, name, profilePath }) {
+export function Card({ id, name, profilePath, handleClick }) {
   
     // store the base URL for images inside a const
     const IMAGE_URL = 'https://image.tmdb.org/t/p/original'
 
     return (
-        <div className='card-container'>
+        <div className='card-container' onClick={() => handleClick(id)}>
             <img className='actor-pic' src={IMAGE_URL + profilePath} alt="actor/actress image" />
             <p className='actor-name'>{name}</p>
         </div>
