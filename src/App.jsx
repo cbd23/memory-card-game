f69a08096e4b38e30707cd3e2920787dffbdc9f8
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-06-19 17:27:48 +0300

    Add handleClick(), addToClicked() and shuffleCards().

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
 
   const [cards, setCards] = useState([])
   const [clickedCards, setClickedCards] = useState([])
-  const [score, setScore] = useState(0)
-  const [bestScore, setBestScore] = useState(0)
+  let [score, setScore] = useState(0)
+  let [bestScore, setBestScore] = useState(0)
   
   const options = {
     method: 'GET',
@@ -31,16 +31,54 @@ function App() {
         setCards(firstTwelveCards)
       })
       .catch(err => console.error(err))
-    }, [])
+  }, [])
 
-    // log the cards array whenever it changes
-    useEffect(() => {
-      console.log(cards)
-    }, [cards])
+  // log the cards array whenever it changes
+  useEffect(() => {
+    console.log(cards)
+  }, [cards])
 
-    function handleClick(id) {
-      console.log(id)
+  // log the score & best score whenever they change
+  useEffect(() => {
+    console.log('SCORE: ' + score)
+    console.log('BEST SCORE: ' + bestScore)
+  }, [score, bestScore])
+
+  // change cards' order & update the list of clickedCards after each click
+  function handleClick(id) {
+    shuffleCards()
+    addToClicked(id)
+    console.log(clickedCards)
+  }
+
+  // verify whether a card was already clicked or not and update/reset the score & best score
+  function addToClicked(card) {
+    if (clickedCards.includes(card)) {
+      console.log(card + ' is already on the list')
+
+      setClickedCards([])
+      setScore(0)
+      console.log('score was reset to 0')
+    } else {
+      setClickedCards([...clickedCards, card])
+      setScore(score += 1)
+
+      score > bestScore ? setBestScore(score) : null
+
+      console.log('SCORE: ' + score)
+      console.log('BEST SCORE: ' + bestScore)
     }
+  }
+
+  // shuffle the array that stores the cards using the Fisher-Yates algorithm
+  function shuffleCards() {
+    const shuffled = [...cards];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    setCards(shuffled);
+  }
 
   return (
     <div className='app-container'>
