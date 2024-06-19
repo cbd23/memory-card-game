48256e75fe70e6387fd04127c8ae5401881065ca
catalinbadila <112496548+cbd23@users.noreply.github.com>
2024-06-19 18:12:47 +0300

    Remove placeholders & update score + bestScore using state.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import './../styles/Header.css'
 
-export function Header() {
+export function Header({ score, bestScore }) {
     return (
         <div className="header-container">
             <p className='rules'>Click as many actors as you can without clicking the same actor twice!</p>
-            <div className='best-score'>BEST SCORE: 4</div>
-            <div className='current-score'>SCORE: 0</div>
+            <div className='best-score'>BEST SCORE: {bestScore}</div>
+            <div className='current-score'>SCORE: {score}</div>
         </div>
     )
 }
